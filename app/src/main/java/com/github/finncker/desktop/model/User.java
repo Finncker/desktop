4533ff9202fdd851be80d4aaa3c53f354fc6187a
Maxsuel <maxsuel.ap.l.santos@gmail.com>
2025-11-20 09:46:57 -0300

    Account update

--- app/src/main/java/com/github/finncker/desktop/model/User.java
+++ app/src/main/java/com/github/finncker/desktop/model/User.java
@@ -1,5 +1,27 @@
 package com.github.finncker.desktop.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+@ToString(exclude = "accounts")
 public class User {
-    
+
+    private String id;
+    private String fullName;
+    private String email;
+
+    @Builder.Default
+    private List<Account> accounts = new ArrayList<Account>();
 }
