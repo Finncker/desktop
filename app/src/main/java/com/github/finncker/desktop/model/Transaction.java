1e3d4a260087e79eec7b2f1930da62be2386d876
Maxsuel <maxsuel.ap.l.santos@gmail.com>
2025-11-20 10:11:19 -0300

    Transaction update

--- app/src/main/java/com/github/finncker/desktop/model/Transaction.java
+++ app/src/main/java/com/github/finncker/desktop/model/Transaction.java
@@ -1,5 +1,36 @@
 package com.github.finncker.desktop.model;
 
+import java.time.LocalDate;
+import java.util.UUID;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+@ToString
 public class Transaction {
     
+    private UUID id;
+    private String categoryId;
+    private double amount;
+    private String description;
+
+    @Builder.Default
+    private LocalDate date = LocalDate.now();
+
+    public Transaction(String id, String categoryId, double amount, String description) {
+        this.id = UUID.randomUUID();
+        this.categoryId = categoryId;
+        this.amount = amount;
+        this.description = description;
+        this.date = LocalDate.now();
+    }
 }
