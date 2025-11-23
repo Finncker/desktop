0c473ba66dc4a0e221586f1af4e791246ca323c3
Maxsuel <maxsuel.ap.l.santos@gmail.com>
2025-11-23 11:46:27 -0300

    Repository Classes

---
+++ app/src/main/java/com/github/finncker/desktop/model/repository/CategoryRepository.java
@@ -0,0 +1,7 @@
+package com.github.finncker.desktop.model.repository;
+
+import com.github.finncker.desktop.model.interfaces.IRepository;
+
+public class CategoryRepository implements IRepository {
+    
+}
