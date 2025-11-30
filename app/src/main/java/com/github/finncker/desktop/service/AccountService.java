d627e4dd65c5e8d2c64937b43273c5488b8109e5
Maxsuel Santos <maxsuel.ap.l.santos@gmail.com>
2025-11-30 10:58:42 -0300

    Implementing logs in the service classes (business logic)

--- app/src/main/java/com/github/finncker/desktop/service/AccountService.java
+++ app/src/main/java/com/github/finncker/desktop/service/AccountService.java
@@ -6,19 +6,25 @@ import com.github.finncker.desktop.model.exceptions.AccountNotFoundException;
 import com.github.finncker.desktop.model.repository.AccountRepository;
 import com.github.finncker.desktop.model.repository.TransactionRepository;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class AccountService {
 
     private AccountRepository accRepo = new AccountRepository();
     private TransactionRepository transRepo = new TransactionRepository();
 
     public Account create(Account acc) {
+        log.info("Criando conta: {}", acc.getName());
         return accRepo.create(acc);
     }
 
     public Account read(String id) throws AccountNotFoundException {
+        log.info("Buscando conta com id = {}", id);
         Account a = accRepo.read(id);
 
         if (a == null) {
+            log.info("Conta não encontrada: id = {}", id);
             throw new AccountNotFoundException(id);
         }
 
@@ -26,30 +32,39 @@ public class AccountService {
     }
 
     public Account update(Account acc) {
+        log.info("Atualizado conta: id = {}", acc.getId());
         return accRepo.update(acc);
     }
 
     public boolean delete(String id) throws AccountNotFoundException {
+        log.info("Tentando deletar conta: id = {}", id);
         boolean deleted = accRepo.delete(id);
 
         if (!deleted) {
+            log.error("Erro ao deletar conta: id = {}", id);
             throw new AccountNotFoundException(id);
         }
 
+        log.info("Conta deletada com sucesso: id = {}", id);
         return true;
     }
 
     public double calculateBalance(Account acc) {
-        return acc.getTransactions()
-                  .stream()
-                  .mapToDouble(Transaction::getAmount)
-                  .sum() + acc.getInitialBalance();
+        double total = acc.getTransactions()
+                          .stream()
+                          .mapToDouble(Transaction::getAmount)
+                          .sum() + acc.getInitialBalance();
+        
+        log.debug("Saldo calculado para conta id = {} : R$ {}", acc.getId(), total);
+
+        return total;
     }
 
     public void addTransaction(Account acc, Transaction t) {
+        log.info("Adicionando transação {} à conta id = {}", t.getId(), acc.getId());
+
         acc.getTransactions().add(t);
         transRepo.create(t);
         accRepo.update(acc);
     }
-
 }
--- app/src/main/java/com/github/finncker/desktop/service/CategoryService.java
+++ app/src/main/java/com/github/finncker/desktop/service/CategoryService.java
@@ -1,36 +1,46 @@
 package com.github.finncker.desktop.service;
 
 import com.github.finncker.desktop.model.repository.CategoryRepository;
+
+import lombok.extern.slf4j.Slf4j;
+
 import com.github.finncker.desktop.model.entities.Category;
 import com.github.finncker.desktop.model.exceptions.CategoryNotFoundException;
 
+@Slf4j
 public class CategoryService {
 
     private CategoryRepository catRepo = new CategoryRepository();
 
     public Category create(Category cat) {
+        log.info("Criando categoria: {}", cat.getName());
         return catRepo.create(cat);
     }
 
-    public Category read(String cat) throws CategoryNotFoundException {
-        Category a = catRepo.read(cat);
+    public Category read(String id) throws CategoryNotFoundException {
+        log.info("Buscando categoria id = {}", id);
+        Category a = catRepo.read(id);
 
         if (a == null) {
-            throw new CategoryNotFoundException(cat);
+            log.warn("Categoria não encontrada: id = {}", id);
+            throw new CategoryNotFoundException(id);
         }
 
         return a;
     }
 
     public Category update(Category cat) {
+        log.info("Atualizando categoria id = {}", cat.getId());
         return catRepo.update(cat);
     }
 
-    public boolean delete(String cat) throws CategoryNotFoundException {
-        boolean deleted = catRepo.delete(cat);
+    public boolean delete(String id) throws CategoryNotFoundException {
+        log.info("Tentando deletar categoria id = {}", id);
+        boolean deleted = catRepo.delete(id);
 
         if (!deleted) {
-            throw new CategoryNotFoundException(cat);
+            log.error("Erro ao deletar categoria: id = {}", id);
+            throw new CategoryNotFoundException(id);
         }
 
         return true;
